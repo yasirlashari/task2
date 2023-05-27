7965a99f4abe756b832a76eb4d975055c0d63398
yasirlashari <yasirlashari131@gmail.com>
2023-05-27 16:59:08 +0500

    Timer Task 2

--- src/App.js
+++ src/App.js
@@ -1,40 +1,89 @@
-//import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
 
-import React from 'react'
-import { useState, useEffect } from 'react';
+const App = () => {
+	// We need ref in this, because we are dealing
+	// with JS setInterval to keep track of it and
+	// stop it when needed
+	const Ref = useRef(null);
 
-const Timer = (props:any) => {
-    const {initialMinute = 0,initialSeconds = 0} = props;
-    const [ minutes, setMinutes ] = useState(initialMinute);
-    const [seconds, setSeconds ] =  useState(initialSeconds);
-    useEffect(()=>{
-    let myInterval = setInterval(() => {
-            if (seconds > 0) {
-                setSeconds(seconds - 1);
-            }
-            if (seconds === 0) {
-                if (minutes === 0) {
-                    clearInterval(myInterval)
-                } else {
-                    setMinutes(minutes - 1);
-                    setSeconds(59);
-                }
-            } 
-        }, 1000)
-        return ()=> {
-            clearInterval(myInterval);
-          };
-    });
+	// The state for our timer
+	const [timer, setTimer] = useState('00:00:00');
 
-    return (
-        <div>
-        { minutes === 0 && seconds === 0
-            ? null
-            : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
-        }
-        </div>
-    )
+	const getTimeRemaining = (e) => {
+		const total = Date.parse(e) - Date.parse(new Date());
+		const seconds = Math.floor((total / 1000) % 60);
+		const minutes = Math.floor((total / 1000 / 60) % 60);
+		const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+		return {
+			total, hours, minutes, seconds
+		};
+	}
+
+	const startTimer = (e) => {
+		let { total, hours, minutes, seconds }
+					= getTimeRemaining(e);
+		if (total >= 0) {
+
+			// update the timer
+			// check if less than 10 then we need to
+			// add '0' at the beginning of the variable
+			setTimer(
+				(hours > 9 ? hours : '0' + hours) + ':' +
+				(minutes > 9 ? minutes : '0' + minutes) + ':'
+				+ (seconds > 9 ? seconds : '0' + seconds)
+			)
+		}
+	}
+
+	const clearTimer = (e) => {
+
+		// If you adjust it you should also need to
+		// adjust the Endtime formula we are about
+		// to code next
+		setTimer('00:00:10');
+
+		// If you try to remove this line the
+		// updating of timer Variable will be
+		// after 1000ms or 1sec
+		if (Ref.current) clearInterval(Ref.current);
+		const id = setInterval(() => {
+			startTimer(e);
+		}, 1000)
+		Ref.current = id;
+	}
+
+	const getDeadTime = () => {
+		let deadline = new Date();
+
+		// This is where you need to adjust if
+		// you entend to add more time
+		deadline.setSeconds(deadline.getSeconds() + 10);
+		return deadline;
+	}
+
+	// We can use useEffect so that when the component
+	// mount the timer will start as soon as possible
+
+	// We put empty array to act as componentDid
+	// mount only
+	useEffect(() => {
+		clearTimer(getDeadTime());
+	}, []);
+
+	// Another way to call the clearTimer() to start
+	// the countdown is via action event from the
+	// button first we create function to be called
+	// by the button
+	const onClickReset = () => {
+		clearTimer(getDeadTime());
+	}
+
+	return (
+		<div className="App">
+			<h2>{timer}</h2>
+			<button onClick={onClickReset}>Reset</button>
+		</div>
+	)
 }
 
-export default Timer;
+export default App;
